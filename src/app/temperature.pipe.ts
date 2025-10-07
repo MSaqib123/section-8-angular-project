17ec443a0e4364b66b44e93f318ed1041efb43ff
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-10-08 02:38:20 +0500

    6. Using Custom Pipes to Perform Custom Transformations

--- src/app/temperature.pipe.ts
+++ src/app/temperature.pipe.ts
@@ -1,5 +1,30 @@
 //#region   5. Building a First Custom Pipe
 
+// import { Pipe, PipeTransform } from "@angular/core";
+
+// @Pipe({
+//     name:'temp',
+// })
+
+// export class TemperaturePipe implements PipeTransform{
+//     //= Pipe Class must need this  Method =
+//     // this is interface which is used by pipes
+//     // implement it with class
+
+//     // value:any    (the value on which the pipe is used)
+//     // value:any    (the Configuration values for the pipe)
+//     transform(value:any, ...args: any[])
+//     {
+//         return value + '-transformed';
+//     }
+// }
+
+//#endregion
+
+
+
+//#region   6. Using Custom Pipes to Perform Custom Transformations 
+
 import { Pipe, PipeTransform } from "@angular/core";
 
 @Pipe({
@@ -7,15 +32,18 @@ import { Pipe, PipeTransform } from "@angular/core";
 })
 
 export class TemperaturePipe implements PipeTransform{
-    //= Pipe Class must need this  Method =
-    // this is interface which is used by pipes
-    // implement it with class
-
-    // value:any    (the value on which the pipe is used)
-    // value:any    (the Configuration values for the pipe)
-    transform(value:any, ...args: any[])
+    
+    transform(value:string | number)
     {
-        return value + '-transformed';
+        let val:number;
+        if(typeof value === 'string'){
+            val = parseFloat(value)
+        }
+        else{
+            val = value;
+        }
+        const outputTmep = val * (9/5) + 32;
+        return `${outputTmep} *F`;
     }
 }
 
