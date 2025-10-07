996c7f03740e0354fbcdfe9520b84ded16989b49
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-10-08 04:08:55 +0500

    11. Pure & Impure Pipes

--- src/app/sort.pipe.ts
+++ src/app/sort.pipe.ts
@@ -1,7 +1,38 @@
+//#region
+// import { Pipe, PipeTransform } from '@angular/core';
+
+// @Pipe({
+//   name: 'sort'
+// })
+// export class SortPipe implements PipeTransform {
+
+//   transform(value: string[] | number[], direction:'asc' | 'desc' = 'asc') {
+//     const sorted = [...value] // seprad operator
+//     sorted.sort((a,b)=>{
+//       if(direction === 'asc'){
+//         return a>b ? 1 : -1;
+//       }
+//       else{
+//         return a>b ? -1 : 1;
+//       }
+//     });
+//     return sorted;
+//   }
+
+// }
+
+//#endregion
+
+
+
+
+//#region  11. Pure & Impure Pipes
+
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'sort'
+  name: 'sort',
+  pure:false
 })
 export class SortPipe implements PipeTransform {
 
@@ -17,5 +48,6 @@ export class SortPipe implements PipeTransform {
     });
     return sorted;
   }
-
 }
+
+//#endregion
